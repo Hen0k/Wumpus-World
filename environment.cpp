fe7478be54843d4f4b5ed1a2391781226a94be9a
Haylemicheal <hmicheal50@gmail.com>
2019-09-15 14:23:55 -0400

    Adgecent rooms to the wumpus and the pit behaviour is known

--- environment.cpp
+++ environment.cpp
@@ -46,14 +46,94 @@ void Environment::set_pits_position(Environment &env){
     }
 }
 void Environment::set_wumpus_position(Environment &env){
-    int remainder = env.index_cont.at(0) % 4;
-    int div = (env.index_cont.at(0) - remainder)/4;
+    env.wumpus_cont = env.index_cont.at(0);
+    int remainder = env.wumpus_cont % 4;
+    int div = (env.wumpus_cont - remainder)/4;
     env.world.at(div).at(remainder) = 'W';
 }
 void Environment::set_gold_position(Environment &env){
-    int remainder = env.index_cont.at(1) % 4;
-    int div = (env.index_cont.at(1) - remainder)/4;
+    env.gold_cont = env.index_cont.at(1);
+    int remainder = env.gold_cont % 4;
+    int div = (env.gold_cont - remainder)/4;
     env.world.at(div).at(remainder) = 'G';
+}
+void Environment::print_world(Environment &env){
+    for(int i=3;i>=0;i--){
+        for(int j=0;j<4;j++){
+            std::cout<<env.world.at(i).at(j)<<" ";
+       }
+      std::cout<<std::endl;
+    }
+}  
+
+void Environment::set_stinch(Environment &env){
+     env.stinch.push_back(env.wumpus_cont + 4);
+     env.stinch.push_back(env.wumpus_cont - 4);
+     env.stinch.push_back(env.wumpus_cont + 1);
+     env.stinch.push_back(env.wumpus_cont - 1);
+     int size = 4;
+     for(int i=0;i<size;i++){
+         if(env.stinch.at(i) < 0 || env.stinch.at(i) > 15){
+             env.stinch.erase(std::remove(env.stinch.begin(), env.stinch.end(), env.stinch.at(i)), env.stinch.end());
+             size--;
+         }
+         if(env.wumpus_cont % 4==0 && env.stinch.at(i)%4==3){
+             env.stinch.erase(std::remove(env.stinch.begin(), env.stinch.end(), env.stinch.at(i)), env.stinch.end());
+             size--;
+         }
+         if((env.wumpus_cont+1) % 4==0 && env.stinch.at(i)%4==0){
+             env.stinch.erase(std::remove(env.stinch.begin(), env.stinch.end(), env.stinch.at(i)), env.stinch.end());
+             size--;
+         }
+         
+     }
+     std::cout<<"Stinch:"<<std::endl;
+     for(int t=0;t<env.stinch.size();t++){
+        std::cout<<env.stinch.at(t)<<" ";
+     }
+     std::cout<<std::endl;
+}
+
+void Environment::set_breeze(Environment &env){
+    
+    
+    for(int j=0;j<env.pits_cont.size();j++){
+        std::vector<int> pit_adj_cont;
+        pit_adj_cont.push_back(env.pits_cont.at(j) - 4);
+        pit_adj_cont.push_back(env.pits_cont.at(j) + 4);
+        pit_adj_cont.push_back(env.pits_cont.at(j) - 1);
+        pit_adj_cont.push_back(env.pits_cont.at(j) + 1);
+        
+        int size = pit_adj_cont.size();
+        
+        for(int i=0;i<size;i++){
+         if(pit_adj_cont.at(i) < 0 || pit_adj_cont.at(i) > 15){
+            pit_adj_cont.erase(std::remove(pit_adj_cont.begin(), pit_adj_cont.end(), pit_adj_cont.at(i)), pit_adj_cont.end());
+            size--;i--;
+            continue;
+         }
+         if(env.pits_cont.at(j) % 4==0 && pit_adj_cont.at(i)%4==3){
+             pit_adj_cont.erase(std::remove(pit_adj_cont.begin(), pit_adj_cont.end(), pit_adj_cont.at(i)), pit_adj_cont.end());
+             size--;i--;
+             continue;
+         }
+         if((env.pits_cont.at(j)+1) % 4==0 && pit_adj_cont.at(i)%4==0){
+             pit_adj_cont.erase(std::remove(pit_adj_cont.begin(), pit_adj_cont.end(), pit_adj_cont.at(i)), pit_adj_cont.end());
+             size--;i--;
+             continue;
+         }
+         
+     }
+        env.breeze.push_back(pit_adj_cont);
+        pit_adj_cont.clear();
+    }
+     std::cout<<"Breeze:"<<std::endl;
+     for(int j=0;j<env.breeze.size();j++){
+     for(int t=0;t<env.breeze.at(j).size();t++){
+        std::cout<<env.breeze.at(j).at(t)<<" ";
+     }
+      std::cout<<std::endl;
+    }
 }
 int main(){
     Environment env;
@@ -62,13 +142,9 @@ int main(){
     env.set_pits_position(env);
     env.set_wumpus_position(env);
     env.set_gold_position(env);
-    for(int i=3;i>=0;i--){
-        for(int j=0;j<4;j++){
-            std::cout<<env.world.at(i).at(j)<<" ";
-      }
-      std::cout<<std::endl;
-    }
-  
-     std::cout<<std::endl;
+    env.print_world(env);
+    env.set_stinch(env);
+    env.set_breeze(env);
+    std::cout<<std::endl;
     return 0;
 }
--- environment.h
+++ environment.h
@@ -10,18 +10,28 @@
 #include <algorithm>
 
 class Environment{
-    public:
+    private:
         int player_start = 0;
         int grid_size = 4;
         std::vector<int> index_cont;
         std::vector<int> pits_cont;
+        int wumpus_cont;
+        int gold_cont;
         std::vector<int>::iterator it; 
+        std::vector<int> stinch;
+        std::vector<std::vector<int>> breeze;
+        std::vector<int> glitter;
         std::vector<std::vector<char>> world;
+    public:
         void set_index_cont(Environment &env);
         void set_rooms(Environment &env);
         void set_wumpus_position(Environment &env);
         void set_pits_position(Environment &env);
         void set_gold_position(Environment &env);
+        void print_world(Environment &env);
+        void set_stinch(Environment &env);
+        void set_breeze(Environment &env);
+        void set_glitter(Environment &env);
         void set_agent();
 };
 #endif
